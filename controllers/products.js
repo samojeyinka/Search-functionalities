44cc7abd2a4c05d81d285005ab296e5f3f995a28
tailorng <cashbankincome@gmail.com>
2023-10-19 00:02:32 -1200

    day 2 store api

--- controllers/products.js
+++ controllers/products.js
@@ -2,15 +2,13 @@ const product = require('../models/product');
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-    const search = 'e';
-    const products = await Product.find({
-        name: {$regex:search, $options:'i'},
-    });
+    
+    const products = await Product.find({}).limit(4)
     res.status(200).json({ products, nbHits: products.length });
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured,company,name}  = req.query;
+    const { featured,company,name,sort,fields}  = req.query;
     const queryObject = {};
 
     if (featured) {
@@ -24,7 +22,27 @@ const getAllProducts = async (req, res) => {
         queryObject.name = {$regex:name, $options:'i'};
     }
     console.log(queryObject)
-    const products = await Product.find(queryObject)
+    let result = Product.find(queryObject);
+if(sort){
+    const sortList = sort.split(',').join(' ');
+   result = result.sort(sortList);
+} else{
+    result = result.sort('createdAt');
+}
+
+if(fields){
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+}
+
+const page = Number(req.query.page) || 1;
+const limit = Number(req.query.limit) || 10;
+const skip = (page - 1) * limit;
+
+result = result.skip(skip).limit(limit);
+ 
+const products = await result;
+
     res.status(200).json({ products, nbHits: products.length });
 }
 
